4928fc602f014010731d20f433eaa1d499ea1045
Jinay <777nightcode@gmail.com>
2024-06-28 14:08:58 +0530

    Recipe app

---
+++ Recipe App/app.js
@@ -0,0 +1,34 @@
+const searchBox = document.querySelector(".searchBox");
+const searchBtn = document.querySelector(".searchBtn");
+const recipeContainer = document.querySelector(".recipe-container");
+
+//function to get recipes
+const fetchRecipes = async (query) => {
+  recipeContainer.innerHTML = "<h2>Fetchig Recipes</h2>";
+  const data = await fetch(
+    `http://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+  );
+  const response = await data.json();
+
+  recipeContainer.innerHTML = "";
+  response.meals.forEach((meal) => {
+    const recipeDiv = document.createElement("div");
+    recipeDiv.classList.add("recipe");
+    recipeDiv.innerHTML = `
+    <img src="${meal.strMealThumb}">
+    <h3>${meal.strMeal}</h3>
+    <p><span>${meal.strArea}</span>Dish</p>
+    <p>Belongs to <span>${meal.strCategory}</span> Category</p>
+    `;
+    const button = document.createElement("button");
+    button.textContent = "View Recipe";
+    recipeDiv.appendChild(button);
+    recipeContainer.appendChild(recipeDiv);
+  });
+};
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const searchInput = searchBox.value.trim();
+  fetchRecipes(searchInput);
+});
