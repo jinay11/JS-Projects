9eba8144aed949ce13f9b3b1637aacc245a17d58
Jinay <777nightcode@gmail.com>
2024-06-30 21:23:51 +0530

    Dictionary app

---
+++ Dictionary App/app.js
@@ -0,0 +1,48 @@
+const form = document.querySelector("form");
+const resultDiv = document.querySelector(".result");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getWordInfo(form.elements[0].value);
+});
+
+const getWordInfo = async (word) => {
+    try{
+
+    
+  const reponse = await fetch(
+    `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+  );
+  const data = await reponse.json();
+  let definitions = data[0].meanings[0].definitions[0];
+  resultDiv.innerHTML = `
+    <h2><strong>Word :</strong> ${data[0].word}</h2>
+    <p class="partOfSeech">${data[0].meanings[0].partOfSpeech}</p>
+    <p><strong>Meaning :</strong> ${
+      definitions.definition === undefined
+        ? "Not Found"
+        : definitions.definition
+    }</p>
+    <p><strong>Example :</strong> ${
+      definitions.example === undefined ? "Not Found" : definitions.definition
+    }</p>
+    <p><strong>Antonyms :</strong></p>
+ `;
+
+  //Fetching Antonyms
+  if (definitions.antonyms.length === 0) {
+    resultDiv.innerHTML += `<span>Not Found</span>`;
+  } else {
+    for (i = 0; i < definitions.antonyms.length; i++) {
+      resultDiv.innerHTML += `<li>${definitions.antonyms[i]}</li>`;
+    }
+  }
+
+  //Adding Read More Button
+  resultDiv.innerHTML += `<div><a href="${data[0].sourceUrls}" target="_blank">Read More</a></div>`
+}
+catch(error){
+    resultDiv.innerHTML = `<p>sorry, the word could not be found</p>`;
+}
+};
+
